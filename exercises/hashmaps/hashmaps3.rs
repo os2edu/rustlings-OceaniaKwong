e2f9213dfa19a1d6beedcbe382fb00ae28bfb105
Oceania <yinglongbit@163.com>
2022-11-10 21:57:32 +0800

    lesson3 finish

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -40,6 +39,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let count = scores.entry(team_1_name.clone()).or_insert(Team {
+            name: team_1_name,
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        count.goals_scored += team_1_score;
+        count.goals_conceded += team_2_score;
+
+        let count = scores.entry(team_2_name.clone()).or_insert(Team {
+            name: team_2_name,
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        count.goals_scored += team_2_score;
+        count.goals_conceded += team_1_score;
     }
     scores
 }
--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -2,7 +2,7 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
 
-const NUMBER:u32 = 3;
+const NUMBER:i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
